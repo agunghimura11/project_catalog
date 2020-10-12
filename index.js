85d16b8a2ae2c68e846ac3f994dbccc0fd468444
agunghimura11 <agung.arisudana@gmail.com>
2020-10-12 11:37:33 +0800

    menambahkan bodyParser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 const __dirname = path.resolve()
 
@@ -10,6 +11,8 @@ const app = express()
 app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html',hbs.__express) // mengganti engine mengarah ke file .html
+
+app.use(bodyParser.urlencoded())
 
 //log incoming request 
 app.use(morgan('combined'))
@@ -30,8 +33,14 @@ app.get('/product', (req,res,next) => {
 })
 
 app.get('/add-product', (req, res, nex) => {
-    res.send(req.query)
+    res.send(req.query) //get menggunakan query
 })
+
+app.post('/add-product', (req, res, nex) => {
+    console.log("Req body",req.body)
+    res.send(req.body) //get menggunakan body
+})
+
 
 app.use((err, req, res, next) => {
     res.send(err.message)
