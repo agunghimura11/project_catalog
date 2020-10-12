c30ca9a776d6bf8f64e52b2aeb2123be5e8d945a
agunghimura11 <agung.arisudana@gmail.com>
2020-10-12 12:09:56 +0800

    Menambahkan add product, menyimpanproduct ke database

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
-import morgan from 'morgan'
+import morgan from 'morgan' //menambahkan morgan
 import bodyParser from 'body-parser'
+//const database = require('./database')
+import { initDatabase, initTable, insertProduct } from './database.js'
 
 const __dirname = path.resolve()
 
 const app = express()
+const db = initDatabase()
+initTable(db)
 
 app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
@@ -14,8 +18,8 @@ app.engine('html',hbs.__express) // mengganti engine mengarah ke file .html
 
 app.use(bodyParser.urlencoded())
 
-//log incoming request 
-app.use(morgan('combined'))
+//log incoming request menggunakan morgan
+app.use(morgan('combined')) 
 
 //serve static file
 app.use('/assets', express.static(__dirname + '/assets'))
@@ -32,13 +36,17 @@ app.get('/product', (req,res,next) => {
     res.render('product')
 })
 
-app.get('/add-product', (req, res, nex) => {
+app.get('/add-product', (req, res, nex) => { // metode get
     res.send(req.query) //get menggunakan query
 })
 
-app.post('/add-product', (req, res, nex) => {
+app.post('/add-product', (req, res, nex) => { //metode post
     console.log("Req body",req.body)
-    res.send(req.body) //get menggunakan body
+    //insert Product
+    insertProduct(db, req.body.name, parseInt(req.body.price), '-')
+
+    //redirect
+    res.redirect('/product') //post menggunakan body
 })
 
 
@@ -47,5 +55,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(8000, () => {
-    console.log('App listen on port 8080')
+    console.log('App listen on port 8000')
 })
